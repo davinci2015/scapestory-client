e353a7af9ebbb886b97a7d25a06a33c9a93d46d8
davinci2015 <davinci02014@gmail.com>
2020-01-28 19:53:09 +0100

    PhotoGrid storybook fix

--- components/molecules/PhotoGrid/PhotoGrid.story.tsx
+++ components/molecules/PhotoGrid/PhotoGrid.story.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import {storiesOf} from '@storybook/react'
 import {PhotoGrid} from 'components/molecules'
 import mock from 'mocks/storybook'
+import {action} from '@storybook/addon-actions'
 
 storiesOf('Molecules | PhotoGrid', module).add('default', () => (
     <PhotoGrid
+        openGallery={action('openGallery')}
         images={[
-            {src: mock.aquascapeImage, alt: 'Image'},
-            {src: mock.aquascapeImage, alt: 'Image'},
-            {src: mock.aquascapeImage, alt: 'Image'},
-            {src: mock.aquascapeImage, alt: 'Image'},
-            {src: mock.aquascapeImage, alt: 'Image'},
-            {src: mock.aquascapeImage, alt: 'Image'},
-            {src: mock.aquascapeImage, alt: 'Image'},
+            {id: 1, src: mock.aquascapeImage, alt: 'Image'},
+            {id: 2, src: mock.aquascapeImage, alt: 'Image'},
+            {id: 3, src: mock.aquascapeImage, alt: 'Image'},
+            {id: 4, src: mock.aquascapeImage, alt: 'Image'},
+            {id: 5, src: mock.aquascapeImage, alt: 'Image'},
+            {id: 6, src: mock.aquascapeImage, alt: 'Image'},
+            {id: 7, src: mock.aquascapeImage, alt: 'Image'},
         ]}
     />
 ))
