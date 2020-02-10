2c93b69f7a840d47010db0875f35bc1683810f85
davinci2015 <davinci02014@gmail.com>
2020-02-10 17:34:28 +0100

    Removed tags from AquascapeCard storybook

--- components/molecules/AquascapeCard/AquascapeCard.story.tsx
+++ components/molecules/AquascapeCard/AquascapeCard.story.tsx
@@ -9,7 +9,6 @@ storiesOf('Molecules | AquascapeCard', module).add('default', () => (
             id={1}
             viewsCount={100}
             likesCount={200}
-            tags={[{name: 'Diorama'}]}
             title="My awesome aquascape"
             image="https://images.homify.com/image/upload/a_0,c_fill,f_auto,h_900,q_auto,w_1920/v1441196948/p/photo/image/745836/360er-aktuell_resize2.jpg"
         />
