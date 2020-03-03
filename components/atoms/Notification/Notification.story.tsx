62a2c74d57e29235d4ab8bc5ebdcb208731290d9
davinci2015 <davinci02014@gmail.com>
2020-03-03 10:39:35 +0100

    Fixed storybook

--- components/atoms/Notification/Notification.story.tsx
+++ components/atoms/Notification/Notification.story.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import {storiesOf} from '@storybook/react'
 
 import Notification from './Notification'
-import mock from 'mocks/storybook'
 import Icon from '../Icon'
 
 export const storyNotification = () => (
-    <Notification image={mock.userImage} createdAt={1473245023718} icon={Icon.HEART_OUTLINE}>
+    <Notification createdAt={1473245023718} icon={Icon.HEART_OUTLINE}>
         <strong>Mark Ronson</strong> commented on your photo in <strong>Beautiful Dream.</strong>
     </Notification>
 )
