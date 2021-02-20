c5326f2475118c8ec2db0a36a5ebf61a6fcb0f60
davinci2015 <danijel.vincijanovic@gmail.com>
2021-02-20 22:38:31 +0100

    Updated Select component

--- components/atoms/Select/Select.story.tsx
+++ components/atoms/Select/Select.story.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
 
-import SelectCreatable from './SelectCreatable'
+import {SelectCreatable, SelectDefault} from './Select'
 
 storiesOf('Atoms | SelectCreatable', module).add('default', () => (
     <SelectCreatable
+        onChange={option => option}
+        options={[
+            {value: 'random', label: 'random'},
+            {value: 'second', label: 'second'},
+        ]}
+    />
+))
+
+storiesOf('Atoms | Select', module).add('default', () => (
+    <SelectDefault
         onChange={option => option}
         options={[
             {value: 'random', label: 'random'},
--- components/atoms/Select/Select.tsx
+++ components/atoms/Select/Select.tsx
@@ -1,5 +1,6 @@
 import React, {CSSProperties} from 'react'
 import CreatableSelect from 'react-select/creatable'
+import Select from 'react-select'
 import {Option} from 'react-select/src/filters'
 import {
     ActionMeta,
@@ -10,7 +11,7 @@ import {
     Theme,
 } from 'react-select'
 
-import {colors} from 'styles'
+import {colors, borderRadius} from 'styles'
 import {NoSSR} from 'components/core'
 
 export interface Props<OptionType> {
@@ -23,9 +24,40 @@ export interface Props<OptionType> {
 }
 
 const styles = {
+    control: (styles: CSSProperties) => ({
+        ...styles,
+        height: 62,
+        borderWidth: 2,
+        borderColor: colors.SHADE_LIGHT,
+        borderRadius: borderRadius.SECONDARY,
+        cursor: 'pointer',
+    }),
     option: (styles: CSSProperties, {isFocused}: any) => ({
         ...styles,
         backgroundColor: isFocused ? colors.PRIMARY_LIGHT : colors.WHITE,
+        color: colors.BLACK,
+        padding: '18px 20px',
+        cursor: 'pointer',
+    }),
+    input: (styles: CSSProperties) => ({
+        ...styles,
+        paddingLeft: 20,
+    }),
+    placeholder: (styles: CSSProperties) => ({
+        ...styles,
+        paddingLeft: 20,
+    }),
+    indicatorSeparator: () => ({
+        display: 'none',
+    }),
+    dropdownIndicator: (styles: CSSProperties, {isFocused}: any) => ({
+        ...styles,
+        paddingRight: 16,
+        color: isFocused ? colors.PRIMARY : colors.MID_GRAY,
+    }),
+    menu: (styles: CSSProperties) => ({
+        ...styles,
+        borderRadius: borderRadius.SECONDARY,
     }),
 }
 const theme = (theme: Theme) => ({
@@ -37,6 +69,7 @@ const theme = (theme: Theme) => ({
         danger: colors.ERROR,
         neutral0: colors.WHITE,
         neutral10: colors.SHADE_EXTRA_LIGHT,
+        neutral30: colors.SHADE_LIGHT,
         neutral50: colors.MID_GRAY,
         neutral90: colors.DARK_GRAY,
     },
@@ -65,4 +98,25 @@ const SelectCreatable = function<OptionType>({
     )
 }
 
-export default SelectCreatable
+const SelectDefault = function<OptionType>({
+    filterOptions,
+    onChange,
+    options,
+    placeholder,
+}: Props<OptionType>) {
+    return (
+        <NoSSR>
+            <Select
+                controlShouldRenderValue={false}
+                onChange={onChange}
+                options={options}
+                filterOption={filterOptions}
+                placeholder={placeholder}
+                styles={styles}
+                theme={theme}
+            />
+        </NoSSR>
+    )
+}
+
+export {SelectCreatable, SelectDefault}
